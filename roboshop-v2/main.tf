a788bd233efd4083d095895a586667263e38ac72
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-20 21:55:13 +0530

    Terraform Creating all Nodes

---
+++ roboshop-v2/main.tf
@@ -0,0 +1,56 @@
+variable "ami" {
+  default = "ami-03265a0778a880afb"
+}
+
+variable "security_groups" {
+  default = [ "sg-0bd976be7cb94bf14" ]
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "zone_id" {
+  default = "Z071049823D2ODLIT0YW9"
+}
+
+variable "components" {
+  default = {
+    frontend = { name = "frontend"}
+    catalogue = { name = "catalogue"}
+    mongodb = { name = "mongobd"}
+    user = { name = "user"}
+    redis = { name = "redis"}
+    cart = { name = "cart"}
+    mysql = { name = "mysql"}
+    shipping = { name = "shipping"}
+    payment = { name = "payment"}
+    rabbitmq = { name = "rabbitmq"}
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_groups
+
+  tags = {
+    Name = lookup(each.value, "name", null)
+  }
+}
+
+//resource "aws_route53_record" "records" {
+//  for_each = var.components
+//  zone_id = var.zone_id
+//  name    = "frontend.rdevops57online.com"
+//  type    = "A"
+//  ttl     = 30
+//  records = [lookup(aws_instance.instance, each.key[""] ]
+//}
+
+//output "instances" {
+//value = aws_instance.instance
+//}
+
+
