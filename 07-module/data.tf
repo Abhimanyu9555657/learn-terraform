4d1b13d6539511b42bfb37f89e2aa6c5c6cc6eeb
OM SAI RAM JI <abhimanyu9555657@gmail.com>
2023-12-26 12:47:01 +0530

    Terraform Basics

--- 07-module/data.tf
+++ 07-module/data.tf
@@ -1,8 +1,15 @@
 data "aws_ami_ids" "ami" {
-  name_regex       = "Centos-*"
+  most_recent = true
+  name_regex       = "Centos-8-Devops-Practice"
   owners           = ["973714476881"]
 }
 
 output "ami" {
   value = "data.aws_ami_ids.ami"
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0bd976be7cb94bf14"]
 }
